01846343eb6fa9ac60a7d7eafcfd60438734f3a8
Jordan Tucker <jordan@trypenny.com>
2022-12-01 11:49:11 +0000

    day1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# advent-of-code-2022
+# Advent of Code 2022 Solutions
+
+- [Day 1 Puzzle](https://adventofcode.com/2022/day/1) - [Part 1 Solution](./src/day1/part1.js) - [Part 2 Solution](./src/day1/part2.js)
---
+++ src/day1/part1.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+​
+// parse the input
+const file = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString();
+​
+// util func
+const sum = arr => arr.reduce((acc, cur) => {
+  acc += cur;
+  return acc;
+}, 0);
+​
+// represent each elf as an array of calories
+const elfs = file.split('\n\n').map(calories => calories.split('\n').map(str => parseInt(str)));
+​
+// reduce each elf to it's totalled calories
+const totaledElfs = elfs.map(elf => sum(elf))
+​
+const largest =  Math.max(...totaledElfs);
+​
+console.log(largest);
