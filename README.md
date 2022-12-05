6ce3726e3bdd56144eda7108eda10f962b71b71e
Jordan Tucker <jordan@trypenny.com>
2022-12-05 10:00:42 +0000

    day5

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - [Day 2 Puzzle](https://adventofcode.com/2022/day/2) - [Part 1 Solution](./src/day2/part1.js) - [Part 2 Solution](./src/day2/part2.js)
 - [Day 3 Puzzle](https://adventofcode.com/2022/day/3) - [Part 1 Solution](./src/day3/part1.js) - [Part 2 Solution](./src/day3/part2.js)
 - [Day 4 Puzzle](https://adventofcode.com/2022/day/4) - [Part 1 Solution](./src/day4/part1.js) - [Part 2 Solution](./src/day4/part2.js)
+- [Day 5 Puzzle](https://adventofcode.com/2022/day/5) - [Part 1 Solution](./src/day5/part1.js) - [Part 2 Solution](./src/day5/part2.js)
---
+++ src/day5/part1.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const path = require("path");
+
+// parse the input
+const file = fs
+  .readFileSync(path.resolve(__dirname, "input.txt"))
+  .toString();
+const lines = file.split("\n");
+
+const getStartingState = (lines) => {
+  const rows = [];
+  for (let line of lines) {
+    if (!line.includes("[")) {
+      break;
+    }
+    rows.push(line.replace(/\s\s\s\s/g, "[-]").replace(/(\s|\[|\])/g, ""));
+  }
+
+  const numRows = rows[0].length; // row length should be uniform
+
+  const state = [];
+  for (let i = 0; i < numRows; i++) {
+    let column = [];
+    for (const row of rows) {
+      column.push(row[i]);
+    }
+    state.push([...column.filter((char) => char !== "-")]);
+  }
+
+  return state;
+};
+
+const executeInstructions = (lines, state) => {
+  for (const line of lines) {
+    if (line.startsWith("move")) {
+      state = executeInstruction(line, state);
+    }
+  }
+  return state;
+};
+
+const executeInstruction = (instruction, state) => {
+  // extract the numbers from the instructions
+  const [a, count, b, from, c, to] = instruction.split(' ');
+
+  for (let i = 0; i < count; i++) {
+    const char = state[from - 1].shift();
+    state[to - 1].unshift(char);
+  }
+
+  return state;
+};
+
+const getMessage = (state) =>
+  state.reduce((acc, col) => {
+    acc += col[0];
+    return acc;
+  }, "");
+
+let state = getStartingState(lines);
+state = executeInstructions(lines, state);
+
+console.log(getMessage(state));
